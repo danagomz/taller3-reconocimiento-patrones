0f62ac691c2d1b2a92cced5d7020cea24aa5eda5
Minsoullie <samvarvalderruten@gmail.com>
2026-05-11 10:35:54 -0500

    Se agrega mi parte del trabajo, funciones mostrar, limpiar y evaluar <3

--- src/main/scala/Newton/package.scala
+++ src/main/scala/Newton/package.scala
@@ -9,21 +9,22 @@ package object Newton {
   case class Resta(termino1: Expr, termino2: Expr) extends Expr
   case class Division(dividendo: Expr, divisor: Expr) extends Expr
   case class Potencia(base: Expr, exponente: Expr) extends Expr
+  case class Expo(e1: Expr, e2: Expr) extends Expr
   case class Logaritmo(argumento: Expr) extends Expr
 
-  // 1. FUNCIÓN MOSTRAR (eta incompleta)
-  def mostrar(expresion: Expr): String = {
-    expresion match {
-      case Numero(valor) => valor.toString
-      case Atomo(variable) => variable.toString
-      case Suma(t1, t2) => s"(${mostrar(t1)} + ${mostrar(t2)})"
-      case Resta(t1, t2) => s"(${mostrar(t1)} - ${mostrar(t2)})"
-      case Producto(f1, f2) => s"(${mostrar(f1)} * ${mostrar(f2)})"
-      case Division(d1, d2) => s"(${mostrar(d1)} / ${mostrar(d2)})"
-      case Potencia(b, e) => s"(${mostrar(b)} ^ ${mostrar(e)})"
-      case Logaritmo(arg) => s"lg(${mostrar(arg)})"
-    }
+  // 1. FUNCIÓN MOSTRAR (ta completa)
+
+  def mostrar(e: Expr): String = e match {
+    case Numero(d) => d.toString
+    case Atomo(x) => x.toString
+    case Suma(e1, e2) => "(" + mostrar(e1) + " + " + mostrar(e2) + ")"
+    case Resta(e1, e2) => "(" + mostrar(e1) + " - " + mostrar(e2) + ")"
+    case Producto(e1, e2) => "(" + mostrar(e1) + " * " + mostrar(e2) + ")"
+    case Division(e1, e2) => "(" + mostrar(e1) + " / " + mostrar(e2) + ")"
+    case Expo(e1, e2) => "(" + mostrar(e1) + " ^ " + mostrar(e2) + ")"
+    case Logaritmo(e1) => "(lg(" + mostrar(e1) + "))"
   }
+
 
   // 2. FUNCIÓN DERIVAR
   def derivar(funcion: Expr, variable: Atomo): Expr = {
@@ -65,13 +66,85 @@ package object Newton {
 
   // 3. FUNCIÓN EVALUAR
   // def evaluar(expresion: Expr, variable: Atomo, valor: Double): Double = ???
-
+  def evaluar(f: Expr, a: Atomo, v: Double): Double = f match {
+    case Numero(d) => d
+    case Atomo(_) => v // único átomo en la fórmula
+    case Suma(e1, e2) => evaluar(e1, a, v) + evaluar(e2, a, v)
+    case Resta(e1, e2) => evaluar(e1, a, v) - evaluar(e2, a, v)
+    case Producto(e1, e2) => evaluar(e1, a, v) * evaluar(e2, a, v)
+    case Division(e1, e2) => evaluar(e1, a, v) / evaluar(e2, a, v)
+    case Expo(e1, e2) => math.pow(evaluar(e1, a, v), evaluar(e2, a, v))
+    case Logaritmo(e1) => math.log(evaluar(e1, a, v))
+  }
   // 4. FUNCIÓN LIMPIAR
   // def limpiar(expresion: Expr): Expr = ???
+
+  def limpiar(f: Expr): Expr = f match {
+    case Numero(d) => Numero(d)
+    case Atomo(x) => Atomo(x)
+
+    // Suma:   0 + e = e ;  e + 0 = e
+    case Suma(e1, e2) =>
+      val l1 = limpiar(e1)
+      val l2 = limpiar(e2)
+      (l1, l2) match {
+        case (Numero(0.0), _) => l2
+        case (_, Numero(0.0)) => l1
+        case _ => Suma(l1, l2)
+      }
+
+    // Resta:  e - 0 = e ;  0 - 0 = 0
+    case Resta(e1, e2) =>
+      val l1 = limpiar(e1)
+      val l2 = limpiar(e2)
+      (l1, l2) match {
+        case (Numero(0.0), Numero(0.0)) => Numero(0.0)
+        case (_, Numero(0.0)) => l1
+        case _ => Resta(l1, l2)
+      }
+
+    // Prod:   0*e = 0 ;  e*0 = 0 ;  1*e = e ;  e*1 = e
+    case Producto(e1, e2) =>
+      val l1 = limpiar(e1)
+      val l2 = limpiar(e2)
+      (l1, l2) match {
+        case (Numero(0.0), _) => Numero(0.0)
+        case (_, Numero(0.0)) => Numero(0.0)
+        case (Numero(1.0), _) => l2
+        case (_, Numero(1.0)) => l1
+        case _ => Producto(l1, l2)
+      }
+
+    // Div:    0/e = 0 ;  e/1 = e
+    case Division(e1, e2) =>
+      val l1 = limpiar(e1)
+      val l2 = limpiar(e2)
+      (l1, l2) match {
+        case (Numero(0.0), _) => Numero(0.0)
+        case (_, Numero(1.0)) => l1
+        case _ => Division(l1, l2)
+      }
+
+    // Expo:   e^0 = 1 ;  e^1 = e ;  0^e = 0 ;  1^e = 1
+    case Expo(e1, e2) =>
+      val l1 = limpiar(e1)
+      val l2 = limpiar(e2)
+      (l1, l2) match {
+        case (_, Numero(0.0)) => Numero(1.0)
+        case (_, Numero(1.0)) => l1
+        case (Numero(0.0), _) => Numero(0.0)
+        case (Numero(1.0), _) => Numero(1.0)
+        case _ => Expo(l1, l2)
+      }
+
+    // Logaritmo: limpia argumento
+    case Logaritmo(e1) => Logaritmo(limpiar(e1))
+  }
 
 
   // 5. FUNCIÓN RAIZNEWTON
   // def raizNewton(funcion: Expr, variable: Atomo, x0: Double, criterioParada: (Expr, Atomo, Double) => Boolean): Double = ???
+  // Hola Gomez
 }
 
 
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -1,15 +1,20 @@
 import Newton._
 
 // DEFINICIÓN DE EXPRESIONES
-val expr1 = Suma(Atomo('x'), Numero(8.0))
+val expr1 = Suma(Atomo('x'), Numero(2.0))
 val expr2 = Producto(Atomo('x'), Atomo('x'))
-val expr3 = Suma(expr1, Potencia(expr2, Numero(7.0)))
+val expr3 = Suma(expr1, Expo(expr2, Numero(5.0)))
 val expr4 = Logaritmo(Atomo('x'))
 val expr5 = Producto(Division(expr1, expr2), Resta(expr3, expr4))
-val expr6 = Potencia(Atomo('x'), Numero(20.0))
+val expr6 = Expo(Atomo('x'), Numero(3.0))
 
 //PRUEBAS MOSTRAR
-
+mostrar(expr1)   // (x + 2.0)
+mostrar(expr2)   // (x * x)
+mostrar(expr3)   // ((x + 2.0) + ((x * x) ^ 5.0))
+mostrar(expr4)   // (lg(x))
+mostrar(expr5)   // (((x + 2.0) / (x * x)) * (((x + 2.0) + ((x * x) ^ 5.0)) - (lg(x))))
+mostrar(expr6)   // (x ^ 3.0)
 
 // PRUEBAS DE DERIVAR
 mostrar(derivar(expr6, Atomo('x')))
@@ -17,19 +22,41 @@ mostrar(derivar(expr2, Atomo('x')))
 mostrar(derivar(expr2, Atomo('y')))
 mostrar(derivar(Suma(Atomo('k'), Producto(Numero(3.0), Atomo('x'))), Atomo('x')))
 
-// FUNCIONES PENDIENTES
 
-// evaluar(Numero(5.0), Atomo('x'), 1.0)
-// evaluar(Atomo('x'), Atomo('x'), 5.0)
-// evaluar(Suma(expr1, expr2), Atomo('x'), 5.0)
-// evaluar(Prod(expr1, expr2), Atomo('x'), 5.0)
-// evaluar(Resta(expr1, expr2), Atomo('x'), 5.0)
-// evaluar(Div(expr1, expr2), Atomo('x'), 5.0)
-// evaluar(Expo(expr1, expr2), Atomo('x'), 5.0)
-// evaluar(Logaritmo(expr1), Atomo('x'), 5.0)
 
-// limpiar(derivar(Suma(Atomo('k'), Prod(Numero(3.0), Atomo('x'))), Atomo('x')))
-// mostrar(limpiar(derivar(Suma(Atomo('k'), Prod(Numero(3.0), Atomo('x'))), Atomo('x'))))
+//PRUEBAS EVALUAR
+mostrar(Numero(5.0))                                  // 5.0
+evaluar(Numero(5.0), Atomo('x'), 1.0)                 // 5.0
+mostrar(Atomo('x'))                                   // x
+evaluar(Atomo('x'), Atomo('x'), 5.0)                  // 5.0
+mostrar(Suma(expr1, expr2))                           // ((x + 2.0) + (x * x))
+evaluar(Suma(expr1, expr2), Atomo('x'), 5.0)          // 32.0
+mostrar(Producto(expr1, expr2))                       // ((x + 2.0) * (x * x))
+evaluar(Producto(expr1, expr2), Atomo('x'), 5.0)      // 175.0
+mostrar(Resta(expr1, expr2))                          // ((x + 2.0) - (x * x))
+evaluar(Resta(expr1, expr2), Atomo('x'), 5.0)         // -18.0
+mostrar(Division(expr1, expr2))                       // ((x + 2.0) / (x * x))
+evaluar(Division(expr1, expr2), Atomo('x'), 5.0)      // 0.28
+mostrar(Expo(expr1, expr2))                           // ((x + 2.0) ^ (x * x))
+evaluar(Expo(expr1, expr2), Atomo('x'), 5.0)          // 1.341068619663965E21
+mostrar(Logaritmo(expr1))                             // (lg((x + 2.0)))
+evaluar(Logaritmo(expr1), Atomo('x'), 5.0)            // 1.9459101490553132
+
+// PRUEBAS LIMPIAR
+
+limpiar(derivar(Suma(Atomo('k'), Producto(Numero(3.0), Atomo('x'))), Atomo('x')))
+// Numero(3.0)
+
+mostrar(limpiar(derivar(Suma(Atomo('k'), Producto(Numero(3.0), Atomo('x'))), Atomo('x'))))
+// 3.0
+
+// Pruebas adicionales de limpiar
+mostrar(limpiar(Suma(Numero(0.0), Atomo('x'))))                // x
+mostrar(limpiar(Producto(Numero(1.0), Atomo('x'))))            // x
+mostrar(limpiar(Producto(Numero(0.0), Atomo('x'))))            // 0.0
+mostrar(limpiar(Expo(Atomo('x'), Numero(1.0))))                // x
+mostrar(limpiar(Expo(Atomo('x'), Numero(0.0))))                // 1.0
+mostrar(limpiar(Division(Numero(0.0), Atomo('x'))))            // 0.0
 
 // def buenaAprox(f: Expr, a: Atomo, d: Double): Boolean = {
 //   evaluar(f, a, d) < 0.001
