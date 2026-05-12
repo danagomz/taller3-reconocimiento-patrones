20a0b442c65a02794dde2d7fd27b92aa14e63fc3
Dana Sofia Gomez Manrique <danagomez2816@gmail.com>
2026-05-11 21:25:14 -0500

    popo

--- src/main/scala/Newton/package.scala
+++ src/main/scala/Newton/package.scala
@@ -5,11 +5,10 @@ package object Newton {
   case class Numero(valor: Double) extends Expr
   case class Atomo(variable: Char) extends Expr
   case class Suma(termino1: Expr, termino2: Expr) extends Expr
-  case class Producto(factor1: Expr, factor2: Expr) extends Expr
+  case class Prod(factor1: Expr, factor2: Expr) extends Expr
   case class Resta(termino1: Expr, termino2: Expr) extends Expr
-  case class Division(dividendo: Expr, divisor: Expr) extends Expr
-  case class Potencia(base: Expr, exponente: Expr) extends Expr
-  case class Expo(e1: Expr, e2: Expr) extends Expr
+  case class Div(dividendo: Expr, divisor: Expr) extends Expr
+  case class Expo(base: Expr, exponente: Expr) extends Expr
   case class Logaritmo(argumento: Expr) extends Expr
 
   // 1. FUNCIÓN MOSTRAR (ta completa)
@@ -19,8 +18,8 @@ package object Newton {
     case Atomo(x) => x.toString
     case Suma(e1, e2) => "(" + mostrar(e1) + " + " + mostrar(e2) + ")"
     case Resta(e1, e2) => "(" + mostrar(e1) + " - " + mostrar(e2) + ")"
-    case Producto(e1, e2) => "(" + mostrar(e1) + " * " + mostrar(e2) + ")"
-    case Division(e1, e2) => "(" + mostrar(e1) + " / " + mostrar(e2) + ")"
+    case Prod(e1, e2) => "(" + mostrar(e1) + " * " + mostrar(e2) + ")"
+    case Div(e1, e2) => "(" + mostrar(e1) + " / " + mostrar(e2) + ")"
     case Expo(e1, e2) => "(" + mostrar(e1) + " ^ " + mostrar(e2) + ")"
     case Logaritmo(e1) => "(lg(" + mostrar(e1) + "))"
   }
@@ -34,31 +33,23 @@ package object Newton {
       case (Atomo(_), _) => Numero(0.0)
       case (Suma(t1, t2), _) => Suma(derivar(t1, variable), derivar(t2, variable))
       case (Resta(t1, t2), _) => Resta(derivar(t1, variable), derivar(t2, variable))
-      case (Producto(f1, f2), _) => Suma(
-        Producto(derivar(f1, variable), f2),
-        Producto(f1, derivar(f2, variable))
+      case (Prod(f1, f2), _) => Suma(
+        Prod(derivar(f1, variable), f2),
+        Prod(f1, derivar(f2, variable))
       )
-
-      case (Division(dividendo, divisor), _) => Division(
+      case (Div(dividendo, divisor), _) => Div(
         Resta(
-          Producto(derivar(dividendo, variable), divisor),
-          Producto(dividendo, derivar(divisor, variable))
+          Prod(derivar(dividendo, variable), divisor),
+          Prod(dividendo, derivar(divisor, variable))
         ),
-        Potencia(divisor, Numero(2.0))
+        Expo(divisor, Numero(2.0))
       )
-
-      case (Logaritmo(arg), _) => Division(
-        derivar(arg, variable),
-        arg
-      )
-      case (Potencia(base, exponente), _) => Producto(
-        Potencia(base, exponente),
+      case (Logaritmo(arg), _) => Div(derivar(arg, variable), arg)
+      case (Expo(base, exponente), _) => Prod(
+        Expo(base, exponente),
         Suma(
-          Division(
-            Producto(derivar(base, variable), exponente),
-            base
-          ),
-          Producto(derivar(exponente, variable), Logaritmo(base))
+          Div(Prod(derivar(base, variable), exponente), base),
+          Prod(derivar(exponente, variable), Logaritmo(base))
         )
       )
     }
@@ -71,8 +62,8 @@ package object Newton {
     case Atomo(_) => v // único átomo en la fórmula
     case Suma(e1, e2) => evaluar(e1, a, v) + evaluar(e2, a, v)
     case Resta(e1, e2) => evaluar(e1, a, v) - evaluar(e2, a, v)
-    case Producto(e1, e2) => evaluar(e1, a, v) * evaluar(e2, a, v)
-    case Division(e1, e2) => evaluar(e1, a, v) / evaluar(e2, a, v)
+    case Prod(e1, e2) => evaluar(e1, a, v) * evaluar(e2, a, v)
+    case Div(e1, e2) => evaluar(e1, a, v) / evaluar(e2, a, v)
     case Expo(e1, e2) => math.pow(evaluar(e1, a, v), evaluar(e2, a, v))
     case Logaritmo(e1) => math.log(evaluar(e1, a, v))
   }
@@ -104,7 +95,7 @@ package object Newton {
       }
 
     // Prod:   0*e = 0 ;  e*0 = 0 ;  1*e = e ;  e*1 = e
-    case Producto(e1, e2) =>
+    case Prod(e1, e2) =>
       val l1 = limpiar(e1)
       val l2 = limpiar(e2)
       (l1, l2) match {
@@ -112,17 +103,17 @@ package object Newton {
         case (_, Numero(0.0)) => Numero(0.0)
         case (Numero(1.0), _) => l2
         case (_, Numero(1.0)) => l1
-        case _ => Producto(l1, l2)
+        case _ => Prod(l1, l2)
       }
 
     // Div:    0/e = 0 ;  e/1 = e
-    case Division(e1, e2) =>
+    case Div(e1, e2) =>
       val l1 = limpiar(e1)
       val l2 = limpiar(e2)
       (l1, l2) match {
         case (Numero(0.0), _) => Numero(0.0)
         case (_, Numero(1.0)) => l1
-        case _ => Division(l1, l2)
+        case _ => Div(l1, l2)
       }
 
     // Expo:   e^0 = 1 ;  e^1 = e ;  0^e = 0 ;  1^e = 1
@@ -143,8 +134,20 @@ package object Newton {
 
 
   // 5. FUNCIÓN RAIZNEWTON
-  // def raizNewton(funcion: Expr, variable: Atomo, x0: Double, criterioParada: (Expr, Atomo, Double) => Boolean): Double = ???
-  // Hola Gomez
+  def raizNewton(funcion: Expr, variable: Atomo, aproximacionInicial: Double, criterioParada: (Expr, Atomo, Double) => Boolean): Double = {
+    var aproximacionActual = aproximacionInicial
+    while (!criterioParada(funcion, variable, aproximacionActual)) {
+      val valorFuncion = evaluar(funcion, variable, aproximacionActual)
+      val derivadaFuncion = derivar(funcion, variable)
+      val valorDerivada = evaluar(derivadaFuncion, variable, aproximacionActual)
+
+      if (valorDerivada == 0.0) return aproximacionActual
+
+      val siguienteAproximacion = aproximacionActual - (valorFuncion / valorDerivada)
+      aproximacionActual = siguienteAproximacion
+    }
+    aproximacionActual
+  }
 }
 
 
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -2,70 +2,62 @@ import Newton._
 
 // DEFINICIÓN DE EXPRESIONES
 val expr1 = Suma(Atomo('x'), Numero(2.0))
-val expr2 = Producto(Atomo('x'), Atomo('x'))
+val expr2 = Prod(Atomo('x'), Atomo('x'))
 val expr3 = Suma(expr1, Expo(expr2, Numero(5.0)))
 val expr4 = Logaritmo(Atomo('x'))
-val expr5 = Producto(Division(expr1, expr2), Resta(expr3, expr4))
+val expr5 = Prod(Div(expr1, expr2), Resta(expr3, expr4))
 val expr6 = Expo(Atomo('x'), Numero(3.0))
 
-//PRUEBAS MOSTRAR
-mostrar(expr1)   // (x + 2.0)
-mostrar(expr2)   // (x * x)
-mostrar(expr3)   // ((x + 2.0) + ((x * x) ^ 5.0))
-mostrar(expr4)   // (lg(x))
-mostrar(expr5)   // (((x + 2.0) / (x * x)) * (((x + 2.0) + ((x * x) ^ 5.0)) - (lg(x))))
-mostrar(expr6)   // (x ^ 3.0)
+// PRUEBAS MOSTRAR
+mostrar(expr1)
+mostrar(expr2)
+mostrar(expr3)
+mostrar(expr4)
+mostrar(expr5)
 
 // PRUEBAS DE DERIVAR
 mostrar(derivar(expr6, Atomo('x')))
 mostrar(derivar(expr2, Atomo('x')))
 mostrar(derivar(expr2, Atomo('y')))
-mostrar(derivar(Suma(Atomo('k'), Producto(Numero(3.0), Atomo('x'))), Atomo('x')))
+mostrar(derivar(Suma(Atomo('k'), Prod(Numero(3.0), Atomo('x'))), Atomo('x')))
+mostrar(derivar(Div(Atomo('x'), Numero(2.0)), Atomo('x')))
 
+// PRUEBAS EVALUAR
+mostrar(Numero(5.0))
+evaluar(Numero(5.0), Atomo('x'), 1.0)
 
+mostrar(Suma(expr1, expr2))
+evaluar(Suma(expr1, expr2), Atomo('x'), 5.0)
 
-//PRUEBAS EVALUAR
-mostrar(Numero(5.0))                                  // 5.0
-evaluar(Numero(5.0), Atomo('x'), 1.0)                 // 5.0
-mostrar(Atomo('x'))                                   // x
-evaluar(Atomo('x'), Atomo('x'), 5.0)                  // 5.0
-mostrar(Suma(expr1, expr2))                           // ((x + 2.0) + (x * x))
-evaluar(Suma(expr1, expr2), Atomo('x'), 5.0)          // 32.0
-mostrar(Producto(expr1, expr2))                       // ((x + 2.0) * (x * x))
-evaluar(Producto(expr1, expr2), Atomo('x'), 5.0)      // 175.0
-mostrar(Resta(expr1, expr2))                          // ((x + 2.0) - (x * x))
-evaluar(Resta(expr1, expr2), Atomo('x'), 5.0)         // -18.0
-mostrar(Division(expr1, expr2))                       // ((x + 2.0) / (x * x))
-evaluar(Division(expr1, expr2), Atomo('x'), 5.0)      // 0.28
-mostrar(Expo(expr1, expr2))                           // ((x + 2.0) ^ (x * x))
-evaluar(Expo(expr1, expr2), Atomo('x'), 5.0)          // 1.341068619663965E21
-mostrar(Logaritmo(expr1))                             // (lg((x + 2.0)))
-evaluar(Logaritmo(expr1), Atomo('x'), 5.0)            // 1.9459101490553132
+mostrar(Prod(expr1, expr2))
+evaluar(Prod(expr1, expr2), Atomo('x'), 5.0)
+
+mostrar(Expo(expr1, expr2))
+evaluar(Expo(expr1, expr2), Atomo('x'), 5.0)
+
+mostrar(Logaritmo(expr1))
+evaluar(Logaritmo(expr1), Atomo('x'), 5.0)
 
 // PRUEBAS LIMPIAR
+limpiar(derivar(Suma(Atomo('k'), Prod(Numero(3.0), Atomo('x'))), Atomo('x')))
+mostrar(limpiar(derivar(Suma(Atomo('k'), Prod(Numero(3.0), Atomo('x'))), Atomo('x'))))
+mostrar(limpiar(Suma(Numero(0.0), Atomo('x'))))
+mostrar(limpiar(Prod(Numero(1.0), Atomo('x'))))
+mostrar(limpiar(Expo(Atomo('x'), Numero(1.0))))
+mostrar(limpiar(Div(Numero(0.0), Atomo('x'))))
 
-limpiar(derivar(Suma(Atomo('k'), Producto(Numero(3.0), Atomo('x'))), Atomo('x')))
-// Numero(3.0)
+// PRUEBAS RAIZNEWTON
+def buenaAprox(f: Expr, a: Atomo, d: Double): Boolean = {
+  math.abs(evaluar(f, a, d)) < 0.001
+}
+val e1 = Resta(Atomo('x'), Numero(5.0))
+val e2 = Resta(Prod(Numero(2.0), Atomo('x')), Numero(8.0))
+val e3 = Resta(Expo(Atomo('x'), Numero(3.0)), Numero(8.0))
+val e4 = Logaritmo(Atomo('x'))
+val e5 = Resta(Expo(Numero(math.E), Atomo('x')), Numero(10.0))
 
-mostrar(limpiar(derivar(Suma(Atomo('k'), Producto(Numero(3.0), Atomo('x'))), Atomo('x'))))
-// 3.0
-
-// Pruebas adicionales de limpiar
-mostrar(limpiar(Suma(Numero(0.0), Atomo('x'))))                // x
-mostrar(limpiar(Producto(Numero(1.0), Atomo('x'))))            // x
-mostrar(limpiar(Producto(Numero(0.0), Atomo('x'))))            // 0.0
-mostrar(limpiar(Expo(Atomo('x'), Numero(1.0))))                // x
-mostrar(limpiar(Expo(Atomo('x'), Numero(0.0))))                // 1.0
-mostrar(limpiar(Division(Numero(0.0), Atomo('x'))))            // 0.0
-
-// def buenaAprox(f: Expr, a: Atomo, d: Double): Boolean = {
-//   evaluar(f, a, d) < 0.001
-// }
-
-// val e1 = Resta(Prod(Atomo('x'), Atomo('x')), Numero(2.0))
-// val e2 = Resta(Prod(Atomo('x'), Atomo('x')), Numero(4.0))
-// val e3 = Suma(Resta(Prod(Atomo('x'), Atomo('x')), Numero(4.0)), Prod(Numero(3.0), Atomo('x')))
-
-// raizNewton(e1, Atomo('x'), 2.0, buenaAprox)
-// raizNewton(e2, Atomo('x'), 2.0, buenaAprox)
-// raizNewton(e3, Atomo('x'), 2.0, buenaAprox)
+raizNewton(e1, Atomo('x'), 10.0, buenaAprox)
+raizNewton(e2, Atomo('x'), 10.0, buenaAprox)
+raizNewton(e3, Atomo('x'), 3.0, buenaAprox)
+raizNewton(e4, Atomo('x'), 2.0, buenaAprox)
+raizNewton(e5, Atomo('x'), 3.0, buenaAprox)
